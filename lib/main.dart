2040cf090e4871281096ea2885d7db387e115ceb
Jonathan <3713684+gsmlg@users.noreply.github.com>
2024-03-27 16:32:22 +0800

    feat: Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'dart:io' show Platform;
+import 'package:twitter_api_v2/twitter_api_v2.dart' as v2;
+
+Future<void> main() async {
+  Map<String, String> envVars = Platform.environment;
+  
+  final twitter = v2.TwitterApi(
+    // bearerToken: 'YOUR_TOKEN_HERE',
+    oauthTokens: v2.OAuthTokens(
+      consumerKey: envVars['CONSUMER_KEY'],
+      consumerSecret: envVars['CONSUMER_SECRET'],
+      accessToken: envVars['ACCESS_TOKEN'],
+      accessTokenSecret: envVars['ACCESS_SECRET'],
+    ),
+  );
+
+  await twitter.tweets.createTweet(
+    text: envVars['MESSAGE'],
+  );
+}
